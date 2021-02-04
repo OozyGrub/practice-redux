738fd8bcf4824b070c24e63adc2043bf7c0d2c23
oozygrub <oozygrub@hotmail.com>
2021-02-04 18:07:09 +0700

    feat(redux): init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore } from "redux";
+
+/** reducer เป็น pure function */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state += action.payload;
+      break;
+    case "SUBTRACT":
+      state -= action.payload;
+      break;
+    default:
+  }
+  return state;
+};
+
+/** store จะเก็บ state */
+const store = createStore(reducer, 15000);
+
+/** ใช้ในการ update ค่า state */
+store.subscribe(() => {
+  console.log("Update Store:", store.getState());
+});
+
+/** ใช้ dispatch ในการเปลี่ยนแปลง state */
+store.dispatch({
+  type: "ADD",
+  payload: 1000
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
